9eb60a02a004bb1e3ff085b6302d1c5a8f0da1a5
Patrick Rock <prock2011@gmail.com>
2013-09-17 15:57:02 -0500

    need to compile with g++

--- Lab1files/lab1_prob5_framework.c
+++ Lab1files/lab1_prob5_framework.c
@@ -26,32 +26,117 @@ run this file as: gcc FileName.c -o ExecutableName -lrt
     #define CLOCKNAME CLOCK_PROCESS_CPUTIME_ID
 #endif
 
+// actuators
+unsigned int actuator_outputs;
+
+// sensors
+unsigned int sensor_inputs;
+
+
+
+
+
+// returns the nth bit in input
+unsigned int get_nth_bit(unsigned int input, unsigned int n) {
+   if((input & (1 << n)) != 0) {
+    return 1;
+   }  
+   return 0; 
+}
+
+// sets the nth bit in input to set
+void set_nth_bit(unsigned int &input, unsigned int n, unsigned int set) {
+  if (set) { // set nth bit to 1
+    input = input | (1 << n);
+  }
+  else { // set nth bit to 0
+    input = input & ~(1 << n);
+  }
+}
+
+// sensors
+unsigned int driver_on_seat() {
+  return get_nth_bit(sensor_inputs, 0);
+}
+unsigned int driver_seat_belt_fastened() {
+  return get_nth_bit(sensor_inputs, 1);
+}
+unsigned int engine_running() {
+  return get_nth_bit(sensor_inputs, 2);
+}
+unsigned int doors_closed() {
+  return get_nth_bit(sensor_inputs, 3);
+}
+unsigned int key_in_car() {
+  return get_nth_bit(sensor_inputs, 4);
+}
+unsigned int door_lock_lever(){
+  return get_nth_bit(sensor_inputs, 5);
+}
+unsigned int brake_pedal() {  
+  return get_nth_bit(sensor_inputs, 6);
+}
+unsigned int car_moving() {
+  return get_nth_bit(sensor_inputs, 7);
+}
+
+// actuators
+unsigned int bell(unsigned int set) {
+  set_nth_bit(actuator_outputs, 0, set);
+}
+unsigned int door_lock_actuator(unsigned int set) {
+  set_nth_bit(actuator_outputs, 1, set);
+}
+unsigned int brake_actuator(unsigned int set) {
+  set_nth_bit(actuator_outputs, 2, set);
+}
+
+unsigned int bell() {
+  return get_nth_bit(actuator_outputs, 0);
+}
+unsigned int door_lock_actuator() {
+  return get_nth_bit(actuator_outputs, 1);
+}
+unsigned int brake_actuator() {
+  return get_nth_bit(actuator_outputs, 2);
+}
 
 //The code segment which implements the decision logic
 inline void control_action(){
 
-// Put your control/decision logic code segments inside this function
-// This is the actual code whose execution time which is being measure
-	
+if(engine_running() && !driver_seat_belt_fastened()) {
+    bell(1);
+  }
+  else if(engine_running() && !doors_closed()) {
+    bell(1);
+  }
+  else bell(0);
 
+  // door logic
+  if (!driver_on_seat() && key_in_car()) {
+    door_lock_actuator(0);
+  }
+  else if(driver_on_seat() && door_lock_lever()) {
+    door_lock_actuator(1);
+  }
+  else door_lock_actuator(0);
+
+  // brake logic
+  if(brake_pedal() && car_moving()) {
+    brake_actuator(1); 
+  }
+  else brake_actuator(0);
 
 }
 
-
 inline void read_inputs_from_ip_if(){
 
-	//place your input code here
-	//to read the current state of the available sensors
-	
+  sensor_inputs = 0x3FF;	
 
 }
 
 inline void write_output_to_op_if(){
-
-	//place your output code here
-    //to display/print the state of the 3 actuators (DLA/BELL/BA)
-	
-
+  printf("bell: %d\ndoor_lock_actuator: %d\nbrake_actuator: %d\n", bell(), door_lock_actuator(), brake_actuator());
 }
 
 
