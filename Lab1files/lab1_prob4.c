181ff95c4a5cd13df1d9cd3beb32bc56301516e4
Patrick Rock <prock2011@gmail.com>
2013-09-17 15:41:18 -0500

    needs testing but done for part 4, time this.

--- Lab1files/lab1_prob4.c
+++ Lab1files/lab1_prob4.c
@@ -30,28 +30,28 @@ unsigned int sensor_inputs;
 
 
 void read_inputs_from_ip_if(){
-  ;
-}
-
-void write_output_to_op_if(){
-  ;
+  sensor_inputs = 0x3FF;
 }
 
 // returns the nth bit in input
 unsigned int get_nth_bit(unsigned int input, unsigned int n) {
-  return input & (1 << n);  // thank you stack overflow
+   if((input & (1 << n)) != 0) {
+    return 1;
+   }  
+   return 0; 
 }
 
 // sets the nth bit in input to set
-unsigned int set_nth_bit(unsigned int input, unsigned int n, unsigned int set) {
+void set_nth_bit(unsigned int &input, unsigned int n, unsigned int set) {
   if (set) { // set nth bit to 1
-    return input | (1 << n);
+    input = input | (1 << n);
   }
   else { // set nth bit to 0
-    return input & ~(1 << n);
+    input = input & ~(1 << n);
   }
 }
 
+// sensors
 unsigned int driver_on_seat() {
   return get_nth_bit(sensor_inputs, 0);
 }
@@ -77,12 +77,60 @@ unsigned int car_moving() {
   return get_nth_bit(sensor_inputs, 7);
 }
 
+// actuators
+unsigned int bell(unsigned int set) {
+  set_nth_bit(actuator_outputs, 0, set);
+}
+unsigned int door_lock_actuator(unsigned int set) {
+  set_nth_bit(actuator_outputs, 1, set);
+}
+unsigned int brake_actuator(unsigned int set) {
+  set_nth_bit(actuator_outputs, 2, set);
+}
+
+unsigned int bell() {
+  return get_nth_bit(actuator_outputs, 0);
+}
+unsigned int door_lock_actuator() {
+  return get_nth_bit(actuator_outputs, 1);
+}
+unsigned int brake_actuator() {
+  return get_nth_bit(actuator_outputs, 2);
+}
+
 
 //The code segment which implements the decision logic
 void control_action(){
-  
+
+  if(engine_running() && !driver_seat_belt_fastened()) {
+    bell(1);
+  }
+  else if(engine_running() && !doors_closed()) {
+    bell(1);
+  }
+  else bell(0);
+
+  // door logic
+  if (!driver_on_seat() && key_in_car()) {
+    door_lock_actuator(0);
+  }
+  else if(driver_on_seat() && door_lock_lever()) {
+    door_lock_actuator(1);
+  }
+  else door_lock_actuator(0);
+
+  // brake logic
+  if(brake_pedal() && car_moving()) {
+    brake_actuator(1); 
+  }
+  else brake_actuator(0);
 
 }
+
+void write_output_to_op_if(){
+  printf("bell: %d\ndoor_lock_actuator: %d\nbrake_actuator: %d\n", bell(), door_lock_actuator(), brake_actuator());
+}
+
 
 /* ---     You should not have to modify anything below this line ---------*/
 
